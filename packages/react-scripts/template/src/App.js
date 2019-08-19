ca63be39664f801ec4dcf12aa4da2e8c97740d2b
Stephen Castle <stephen@callsignmedia.com>
2019-08-19 16:55:49 -0700

    create welcome template

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -4,30 +4,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img
-        className="w-full"
-        src="/img/card-top.jpg"
-        alt="Sunset in the mountains"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #photography
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #travel
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-          #winter
-        </span>
+    <div class="flex flex-wrap bg-gray-200 h-screen items-center justify-center">
+      <div className="flex-1 max-w-sm rounded overflow-hidden shadow-lg bg-gray-100">
+        <img
+          className="w-full"
+          src="/card-top.jpg"
+          alt="Sunset in the mountains"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">
+            Code Workshop React Starter
+          </div>
+          <p className="text-gray-700 text-base">
+            This placeholder is a react starter with preconfigured TailwindCSS
+            for fast styles.
+          </p>
+        </div>
+        <div className="px-6 py-4">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #Code Workshop
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #TailwindCSS
+          </span>
+        </div>
       </div>
     </div>
   );
